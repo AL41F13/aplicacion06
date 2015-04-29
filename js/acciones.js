ca461b415eae00c28cbba98e9155d9b98a5bb825
AL41F13 <janeth982210@gmail.com>
2015-04-29 13:50:08 -0500

    seis
    eventos

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,18 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+$('#btnvibrar').on('tap',function(){
+	navigator.vibrate(1000);
+});//tap a btnvibrar
+$('#btnbeep').on('tap',function(){
+	navigator.beep(1);
+});//tap a btnbeep
+$('#izquierda').on('swipeleft', function (){
+	alert ("barrio a la izquierda ");
+}); 
+$('#derecha').on('swiperight', function (){
+	alert ("barrio a la derecha ");
+}); 
 }); 
 });
 
