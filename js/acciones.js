cc705b3a6fdc33cb26d4af8f801e12497c39a6e1
AL41F13 <janeth982210@gmail.com>
2015-05-06 13:52:26 -0500

    nueve
    eventos

--- js/acciones.js
+++ js/acciones.js
@@ -13,6 +13,18 @@ $('#izquierda').on('swipeleft', function (){
 $('#derecha').on('swiperight', function (){
 	alert ("barrio a la derecha ");
 }); 
+document.addEventListener("pause", function (){
+	$('#listado').append("<p>la aplicacion se pauso </p>");
+});
+document.addEventListener("resume", function (){
+	$('#listado').append("<p>se reinicio la aplicacion </p>");
+});
+$(window).on('orentionchange',function(e){
+	
+	$('#listado').append("<p> orientacion: " + e.orientecion + "</p>");
+	
+	
+	});
 }); 
 });
 
